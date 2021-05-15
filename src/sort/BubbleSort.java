871d87b5be007672042e7022b0d9b18d5e638ca3
xuxuan11 <xuxuan4396@gmail.com>
2021-05-16 03:55:04 +0900

    insert and merge

--- src/sort/BubbleSort.java
+++ src/sort/BubbleSort.java
@@ -1,7 +1,18 @@
 package sort;public class BubbleSort {
-    public static void bubbleSort(int []arr){
+    public static void main(String[] args) {
+        int[] arrs = {3,4,5,123,45,13,435,765,12,1,2,3,345,56,78,9,3};
+        int[] bubblesort = bubbleSort(arrs);
+        for (int arr : bubblesort) {
+            System.out.println(arr);
+        }
+
+
+    }
+
+
+    public static int[] bubbleSort(int []arr){
         if(arr==null ||arr.length<2){
-            return;
+            return arr;
         }
         for(int e=arr.length-1;e>0;e--){
             for(int i=0;i<e;i++){
@@ -11,6 +22,7 @@ package sort;public class BubbleSort {
                 }
             }
         }
+        return arr;
     }
 
 
--- src/sort/SelectionSort.java
+++ src/sort/SelectionSort.java
@@ -1,29 +1,38 @@
 package sort;
 
 public class SelectionSort {
+    public static void main(String[] args) {
 
-    public static void selectionSort (int []arr){
-        if(arr==null){
+        int[] arrs = {3, 4, 5, 123, 45, 13, 435, 765, 12, 1, 2, 3, 345, 56, 78, 9, 3};
+        selectsort(arrs);
+    }
+
+
+    public static void selectsort(int[] arrs){
+        if (arrs ==null || arrs.length<2){
             return;
         }
-        for (int i=0;i<arr.length-1;i++){
-            int minIndex=i;
-            for(int j=i+1;j<arr.length;j++){
-                minIndex=arr[j]<arr[minIndex]? j: minIndex;
-
+        for (int i = 0; i < arrs.length - 1; i++) {
+            int minIndex =i;
+            for (int j= i+1;j<arrs.length;j++){
+                minIndex = arrs[j]<arrs[minIndex] ? j : minIndex;
             }
-            swap(arr,i,minIndex);
+            swap(arrs, i, minIndex);
+        }
+        for (int arr : arrs) {
+            System.out.println(arr);
+        }
 
+    }
+
+
+        public static void swap ( int arr[], int i, int j){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
 
         }
 
 
-    }
 
-    public static void swap(int arr[],int i,int j){
-        int temp=arr[i];
-        arr[i]=arr[j];
-        arr[j]=temp;
-
-    }
 }
